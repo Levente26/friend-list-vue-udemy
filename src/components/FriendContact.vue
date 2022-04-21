3e1c59e45630ea62109f96975cbb29aed7932841
Levente26 <madi.levente26@gmail.com>
2022-04-21 21:38:34 +0200

    initial commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,72 @@
+<template>
+    <li>
+        <h2>{{ name }} {{ isFavourite ? '(Favourite)' : '' }}</h2>
+        <button @click="toggleDetails">
+            {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+        </button>
+        <button @click="toggleFavourite">
+            Toggle favourite
+        </button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone: </strong>{{ phoneNumber }}</li>
+            <li><strong>Email: </strong>{{ emailAddress }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    // props: ['name','phoneNumber','emailAddress','isFavourite'],
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: true
+        },
+        emailAddress: {
+            type: String,
+            required: true
+        },
+        isFavourite: {
+            type: Boolean,
+            required: false,
+            default: false,
+            // validator: (value) => {
+            //     return value === '1' || value === '0';
+            // }
+        }
+    },
+    // emits: ['toggle-favourite'],
+    emits: {
+        'toggle-favorite': (id) => {
+            if(id) {
+                return true;
+            } else {
+                console.warn('ID is missing')
+                return false;
+            }
+        }
+    },
+    data() {
+        return {
+            detailsAreVisible: false,
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        },
+        toggleFavourite() {
+            // this.friendIsFavourite = !this.friendIsFavourite;
+            this.$emit('toggle-favourite', this.id);
+        }
+    }
+};
+</script>
